db964f282653da9b516080900d892006ca2a290d
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-12-03 21:25:48 +0900

    first commit

---
+++ src/main/java/com/whale/gather_one/GatherOneApplication.java
@@ -0,0 +1,13 @@
+package com.whale.gather_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatherOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whale/gather_one/GatherOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whale.gather_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
